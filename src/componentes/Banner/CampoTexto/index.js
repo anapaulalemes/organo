4fcdf40c19918a0d850f872826537a7540fd14be
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-10-05 03:10:48 -0300

    Adicionando o banner

--- src/componentes/Banner/CampoTexto/index.js
+++ src/componentes/Banner/CampoTexto/index.js
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder='Digite seu nome'/>
+            <label>{props.label}</label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
