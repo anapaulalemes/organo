0639b6c6e5d860b01b7096b52193f747576ada82
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-10-06 03:42:46 -0300

    Adicionando o rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,25 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">
+        <section>
+            <ul>
+                <li>
+                    <a href="https://instagram.com/anapaulalemes" target="_blank">
+                        <img src="/imagens/ig.png" alt="" />
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>
+                Desenvolvido por Ana Paula Lemes
+            </p>
+        </section>
+    </footer>)
+}
+
+export default Rodape
