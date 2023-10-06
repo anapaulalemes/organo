a96c4a7a9070f38a16e35454dbbf0ef0662c2421
anapaulalemes <anapaulal.fredrich@gmail.com>
2023-10-06 18:54:45 -0300

    Corrigindo bugs

--- src/componentes/Seletor/index.js
+++ src/componentes/Seletor/index.js
@@ -4,7 +4,8 @@ const Seletor = (props) => {
     return (
         <div className='seletor'>
             <label>{props.label}</label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.value}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                <option value=''></option>
                 {props.itens.map(item => { return <option key={item}>{item}</option>})}
             </select>
         </div>
